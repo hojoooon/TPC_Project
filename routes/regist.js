081955ea9d3e50aa972785e7198c69b54482111d
miknuyag <74401063+miknuyag@users.noreply.github.com>
2021-02-27 23:15:54 +0900

    Update regist.js

--- routes/regist.js
+++ routes/regist.js
@@ -2,17 +2,14 @@ const express = require('express');
 
 const router = express.Router();
 
-const options = {
-    title: 'Regist',
-    menus: [
-        { title: 'Cookie', link: '#'},
-        { title: 'URL', link: '#'},
-        { title: 'JSON', link: '#'}
-    ]
-}
+const menus = [
+    { title: 'Cookie', link: '#'},
+    { title: 'URL', link: '/regist/url'},
+    { title: 'JSON', link: '#'}
+]
 
 router.get('/', (req, res) => {
-    res.render('regist', options);
+    res.render('regist', { title: 'Regist', menus });
 });
 
 module.exports = router;
